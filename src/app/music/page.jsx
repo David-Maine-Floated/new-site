04f3bb0e90e754eef5bfc0d3eafbf431c34ba463
David <dmaine6711@gmail.com>
2024-04-01 17:09:22 -0400

    finish music page styling

--- src/app/music/page.jsx
+++ src/app/music/page.jsx
@@ -6,11 +6,11 @@ const Music = () => {
           When I'm not coding I'm making Music
         </p>
       </div>
-      <div className="flex justify-around flex-wrap mt-[75px]">
+      <div className="flex justify-around flex-wrap mt-[75px] ">
         <a
           href="https://open.spotify.com/artist/0x4xCoWaOFd3WsKarzaxnW?si=zs1qwjt3QKGBLPoy8mlbHQ"
           target="_blank"
-          className="mb-10"
+          className="mb-10 hover:translate-x-1 hover:-translate-y-1  transition-transform duration-300"
         >
           <div className="flex justify-center">
             <p className="text-lg">Frankie Cosmos</p>
@@ -22,7 +22,7 @@ const Music = () => {
           />
         </a>
         <a
-          className="mb-10"
+          className="mb-10 hover:translate-x-1 hover:-translate-y-1  transition-transform duration-300"
           href="https://open.spotify.com/artist/4440TTGblbIlIldFRFVktf?si=UgyAli1zSa6PXpNLduzH7g"
           target="_blank"
         >
