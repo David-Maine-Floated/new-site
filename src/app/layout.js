c71b6f464ee38086afb69358c98b98e12d92ed82
David <dmaine6711@gmail.com>
2024-05-28 17:45:34 -0400

    new mobile menu, all options in menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,8 +36,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex justify-center w-screen h-lvh box-border fixed">
-        <nav className="invisible sm:visible flex w-full flex-wrapfixed justify-around h-[125px] sm:justify-normal sm:flex-col bg-slate-100 text-blue sm:w-[200px] sm:h-screen sm:bg-purple z-10">
+      <body className="flex justify-center w-screen h-lvh box-border fixed flex-col sm:flex-row">
+        <nav className="hidden sm:visible sm:flex w-full flex-wrapfixed justify-around h-[125px] sm:justify-normal sm:flex-col bg-slate-100 text-blue sm:w-[200px] sm:h-screen sm:bg-purple z-10">
           <NavPageWrapper>
             <div className=" w-[200px] sm:mt-20 flex justify-center ">
               <Drawer>
@@ -121,15 +121,30 @@ export default function RootLayout({ children }) {
 
         <div className="visible sm:hidden">
           <Drawer>
-            <div className="flex justify-center">
+            <div className="flex justify-left">
               <DrawerTrigger className="text-white w-[150px] h-[35px] text-sm rounded bg-slate-700">
                 {/* <Button style={{ width: '150px' }}> */}
-                Contact Info
+                Menu
                 {/* </Button> */}
               </DrawerTrigger>
             </div>
             <DrawerContent>
               <DrawerHeader>
+                <DrawerDescription>
+                  <Button className="bg-slate-700">
+                    <a href="projects">Featured Work</a>
+                  </Button>
+                </DrawerDescription>
+                <DrawerDescription>
+                  <Button className="bg-slate-700">
+                    <a href="music">Music</a>
+                  </Button>
+                </DrawerDescription>
+                <DrawerDescription>
+                  <Button className="bg-slate-700">
+                    <a href="/">Home</a>
+                  </Button>
+                </DrawerDescription>
                 <DrawerTitle>Let's Connect!</DrawerTitle>
                 <DrawerDescription>
                   <Button className="bg-slate-700">
